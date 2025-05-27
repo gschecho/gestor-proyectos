e7d9b50932296b4870e40819c25592b6686d3fd0
Gon <gschecho@gmail.com>
2025-05-27 14:22:33 +0200

    sin FXML

--- README.txt
+++ README.txt
@@ -1,2 +1,3 @@
 Descargar el SDK de javaFX https://gluonhq.com/products/javafx/
-Añadir a la configuración de la VM El path donde se ha descargado y descomprimido el sdk de javafx --module-path .../JavaFX_SDK/javafx-sdk-21.0.7/lib --add-modules javafx.controls,javafx.fxml
+Añadir a la configuración de la VM El path donde se ha descargado y descomprimido el sdk de javafx
+--module-path .../JavaFX_SDK/javafx-sdk-21.0.7/lib --add-modules javafx.controls,javafx.fxml
--- src/main/java/com/springBoot_javaFXS_base/MainApp.java
+++ src/main/java/com/springBoot_javaFXS_base/MainApp.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -15,9 +17,12 @@ public class MainApp extends Application {
     @Override
     public void init() throws Exception {
         // Lanza Spring Boot en segundo plano (antes de cargar la GUI)
+        //init se lanza antes que start y asi springboot no bloquea el hilo principal que es el que utiliza javaFX
+        // Otra opcion sería ejecutar spring en un nuevo hilo.
         context = new SpringApplicationBuilder(GestionSpringJavaFxApplication.class).run();
     }
 
+    /*
     @Override
     public void start(Stage primaryStage) throws Exception {
         // Carga FXML con el controlador inyectado por Spring
@@ -26,7 +31,27 @@ public class MainApp extends Application {
         primaryStage.setScene(new Scene(loader.load()));
         primaryStage.setTitle("App JavaFX + Spring Boot");
         primaryStage.show();
+    }*/
+
+    @Override
+    public void start(Stage stage) {
+
+        final int WIDTH = 600;
+        final int HEIGHT = 400;
+
+
+        Label label = new Label("¡Hola desde JavaFX sin FXML!");
+        label.setLayoutX(100); // posición horizontal
+        label.setLayoutY(150); // posición vertical
+
+        Pane root = new Pane(label); // permite posición absoluta
+
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
+        stage.setScene(scene);
+        stage.setTitle("Aplicación JavaFX");
+        stage.show();
     }
+
 
     @Override
     public void stop() throws Exception {
