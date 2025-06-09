6a1ac7e358f120bc6bf4c4c62e23735938978e7d
Gonzalo Scarpa <gschecho@gmail.com>
2025-06-09 23:38:58 +0200

    cambios main fxml

--- src/main/java/com/springBoot_javaFXS_base/service/PersonService.java
+++ src/main/java/com/springBoot_javaFXS_base/service/PersonService.java
@@ -10,8 +10,12 @@ import java.util.List;
 @Service
 public class PersonService {
 
-    @Autowired
+
     PersonRepository personRepository;
+
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
 
     public List<Person> findAllPersons(){
         return personRepository.findAll();
